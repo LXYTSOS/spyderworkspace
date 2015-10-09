4c04e6e34972ed8891c3b1279b2c85b3ecec6c88
LXYTSOS <834287447@qq.com>
2015-10-09 12:37:15 +0800

    相关性
    计算预测值与真实值的相关性

--- MachineLearningPractice/LinearRegression/regression.py
+++ MachineLearningPractice/LinearRegression/regression.py
@@ -36,11 +36,14 @@ if __name__ == '__main__':
     xMat = mat(xArr)
     yMat = mat(yArr)
     yHat = xMat*ws
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
-    xCopy = xMat.copy()
-    xCopy.sort(0)
-    yHat=xCopy*ws
-    ax.plot(xCopy[:,1], yHat)
-    plt.show()
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111)
+#    ax.scatter(xMat[:,1].flatten().A[0], yMat.T[:,0].flatten().A[0])
+#    xCopy = xMat.copy()
+#    xCopy.sort(0)
+#    yHat=xCopy*ws
+#    ax.plot(xCopy[:,1], yHat)
+#    plt.show()
+    
+    print corrcoef(yHat.T, yMat)
+    
