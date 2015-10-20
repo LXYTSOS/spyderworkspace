2d981087fdf06b8e43950a2d278c5ea2fcb35e2c
LXYTSOS <834287447@qq.com>
2015-10-20 10:47:20 +0800

    modify the comments

--- MachineLearningPractice/LinearRegression/Test.py
+++ MachineLearningPractice/LinearRegression/Test.py
@@ -7,7 +7,7 @@ Created on Tue Oct 20 10:04:41 2015
 
 from numpy import *
 
-#question about regresion formula, why when translate xMat, we still need to
+#question about regresion formula, why when transpose xMat, we still need to
 #translate mat(yArr)
 #This is what happend to xArr
 a = [[1,2,3],[4,5,6],[7,8,9]]
@@ -16,7 +16,7 @@ b = [1,2,3]
 #So When transfer xArr to matrix, it will be 3*3
 m = mat(a)
 #While transger yArr to matrix, it is 1*3
-#So when we mutiply xMat with yMat, We must translate yMat to 3*1
+#So when we mutiply xMat with yMat, We must transpose yMat to 3*1
 #Then it is xMat * yMat.T
 n = mat(b).T
 print m*n
