586958f9f001b0821a8170ae67bf95eaa2f5e2ef
LXYTSOS <834287447@qq.com>
2016-06-06 12:23:00 +0800

    sin
    plot sin shaped wave

---
+++ ScientificComputing/Chapter01/drawSin.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  6 12:18:25 2016
+
+@author: liuxiangyu
+"""
+
+import numpy as np
+import scipy as sp
+import pylab as pl
+
+x = np.linspace(0, 4*np.pi, 100)
+pl.plot(x, np.sin(x))
