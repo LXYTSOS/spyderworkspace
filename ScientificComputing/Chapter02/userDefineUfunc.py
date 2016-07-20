f57fbcb46443a697e3f3988119fb2e183470074c
LXYTSOS <834287447@qq.com>
2016-07-20 22:44:43 +0800

    自定义ufunc以及matplotlib绘图

---
+++ ScientificComputing/Chapter02/userDefineUfunc.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 20 22:08:29 2016
+
+@author: liuxiangyu
+"""
+
+import numpy as np
+
+def triangle_wave(x, c, c0, hc):
+    x = x - int(x)
+    if x >= c:
+        r = 0.0
+    elif x< c0:
+        r = x / c0 *hc
+    else:
+        r = (c-x) / (c-c0) *hc
+    return r
+
+x = np.linspace(0,2,1000)
+y1 = np.array([triangle_wave(t, 0.6, 0.4, 1.0) for t in x])
+
+triangle_ufunc1 = np.frompyfunc(triangle_wave,4,1)
+y2 = triangle_ufunc1(x,0.6,0.4,1.0)
+
+triangle_ufunc2 = np.vectorize(triangle_wave,otypes=[np.float])
+y3 = triangle_ufunc2(x, 0.6, 0.4, 1.0)
+
+print np.all(y1==y2)
+print np.all(y2==y3)
+
+x,y = np.ogrid[:5,:5]
+print x
+print y
+
+x,y = np.mgrid[:5,:5]
+print x
+print y
